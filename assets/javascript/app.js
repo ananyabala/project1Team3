85d2591ceee8b7593893ff03dd367527d4c53d2c
Ananya Bala <ananyabala42@gmail.com>
2018-05-31 20:17:14 -0400

    finished the grid functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,68 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCgKPddkgj3RhPS1B2Hw_DihvXgZBbxxiE",
-    authDomain: "project1team3-39b4f.firebaseapp.com",
-    databaseURL: "https://project1team3-39b4f.firebaseio.com",
-    projectId: "project1team3-39b4f",
-    storageBucket: "project1team3-39b4f.appspot.com",
-    messagingSenderId: "74591335518"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyCgKPddkgj3RhPS1B2Hw_DihvXgZBbxxiE",
+  authDomain: "project1team3-39b4f.firebaseapp.com",
+  databaseURL: "https://project1team3-39b4f.firebaseio.com",
+  projectId: "project1team3-39b4f",
+  storageBucket: "project1team3-39b4f.appspot.com",
+  messagingSenderId: "74591335518"
+};
+firebase.initializeApp(config);
 
-  // API Integration
-  // var searchTerm="tofu";
-  // var appID = "b0ffc540";
+// API Integration
+// Get console.log to work
+// Create a new row -- getting the things to just add, and then seeing
 
-  var queryURL = "https://api.edamam.com/search?q=chicken&app_id=540719b1&app_key=2d98d59eaf4976edb2d1abd92540e167"
 
+$("button").on("click", function () {
+
+  var searchTerm = $("#searchInput").val();
+  console.log(searchTerm);
+
+  var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=b0ffc540&app_key=a41b1a4d8560dbb27bbf0ef650ec9bf0";
+  console.log(queryURL);
 
   $.ajax({
     url: queryURL,
     method: "GET",
-  })
-  .then(function(response){
-    console.log(response);
-  }
-  )
+  }).then(function (response) {
+    for (var i = 0; i < 10; i++) {
+      console.log(response);
+
+      console.log(response.hits[i].recipe.image);
+
+      // Creating the big square for the grid (main)
+      var recipeDiv = $("<div>").attr("class", "card");
+            
+      // Image
+      var recipeImage = $('<img src="' + response.hits[i].recipe.image + '" >');
+      recipeDiv.append(recipeImage);
+
+      // The card body contains the title of the recipe
+            var recipeBodyDiv = $("<div>").attr("class", "card-body");
+            recipeDiv.append(recipeBodyDiv);
+
+      // Title of Recipe
+      var recipeTitle = $("<h5>").text(response.hits[i].recipe.label);
+      recipeBodyDiv.append(recipeTitle);
+
+      // console.log(recipeDiv);
+      $(".grid").append(recipeDiv);
+    }
+
+  });
+  // recipeDiv.append(recipeImage);
+  // $("#imageHere").append(recipeDiv);
+
+});
+
+
+
+
+
+
+
+
+
+
+
